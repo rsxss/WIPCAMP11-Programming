c7a4e95b95709c5cfb447e81ec08f946c8b58163
rsxss <natworpong.ls@mail.kmutt.ac.th>
2019-05-31 08:44:12 +0700

    Add project

---
+++ WIP-XI/src/V/Selection/_3_Exercise.java
@@ -0,0 +1,31 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package V.Selection;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _3_Exercise {
+    public static void main(String[] args) {
+        int x = 4;
+        
+        if(x >3){
+            
+            if(x >0) {
+                x = 7;
+            } else {
+                x = 5;
+            }
+            
+        } else {
+            x=10;
+        }
+        
+        
+        
+        System.out.println(x);
+    }
+}
